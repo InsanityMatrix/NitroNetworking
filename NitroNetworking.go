d5b7a03528ddba8de604abb9f084209741c6472c
InsanityMatrix <39423022+InsanityMatrix@users.noreply.github.com>
2020-02-05 09:54:52 -0500

    Initial Commit

---
+++ NitroNetworking.go
@@ -0,0 +1,32 @@
+﻿package main
+
+import (
+    "fmt"
+    "net/https"
+    "log"
+    "html/template"
+    "github.com/gorilla/mux"
+    "github.com/google/gopacket/pcap"
+)
+
+type App struct {
+    Title string
+    Author string
+}
+func main() {
+    router := mux.NewRouter().StrictSlashes(true)
+    router.HandleFunc("/",IndexHandler)
+    router.HandleFunc("/",NetworkHandler)
+
+    log.Fatal(http.ListenAndServe(":8080",router))
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+    //Load index template
+    t, err := template.ParseFile("html/index.html")
+    if err != nil {
+        fmt.Fprintf(w, "Error parsing template file.")
+    }
+    info := App{Title:"Rascal Networking", Author: "David Piedra"}
+    t.exec(w, info)
+}
