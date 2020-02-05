be16f889f9f18f9e676091542da4f5509e482282
InsanityMatrix <39423022+InsanityMatrix@users.noreply.github.com>
2020-02-05 10:31:09 -0500

    Added Network Devices Function Handler

--- NitroNetworking.go
+++ NitroNetworking.go
@@ -8,7 +8,10 @@ import (
     "github.com/gorilla/mux"
     "github.com/google/gopacket/pcap"
 )
-
+type DeviceList struct {
+    Content string
+    DeviceCount int
+}
 type App struct {
     Title string
     Author string
@@ -16,7 +19,7 @@ type App struct {
 func main() {
     router := mux.NewRouter().StrictSlashes(true)
     router.HandleFunc("/",IndexHandler)
-    router.HandleFunc("/",NetworkHandler)
+    router.HandleFunc("/Network",NetworkHandler)
 
     log.Fatal(http.ListenAndServe(":8080",router))
 }
@@ -28,5 +31,39 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Error parsing template file.")
     }
     info := App{Title:"Rascal Networking", Author: "David Piedra"}
-    t.exec(w, info)
+    t.Execute(w, info)
+}
+
+func NetworkHandler(w http.ResponseWriter, r *http.Request) {
+    t, err := template.ParseFile("html/network.html")
+    if err != nil {
+        fmt.Fprintf(w, "Error parsing network file.")
+    }
+    content := ListDevices()
+    
+    t.Execute(w, content)
+}
+
+func ListDevices() DeviceList {
+    dList := "<div class='dev-list'>\n"
+    //Use pcap to get all devices on the network
+    devices, err := pcap.FindAllDevs()
+    if err != nil {
+        log.Fatal(err)
+    }
+    count := 0
+    for _, device := range devices {
+        dList += "<div class='dev-desc'>\n" + device.Description + "\n</div>"
+        dList += "<div class='dev-name'>\n" + device.Name + "\n</div>"
+        dList += "<ul class='dev-addresses'>\n"
+        for _, address := range device.Addresses {
+            dList += "<li>IP: " + address.IP.String() + " | Subnet mask: " + address.Netmask.String() + "</li>"
+        }
+        dList += "</ul>"
+        count++
+    }
+    dList += "</div>\n"
+
+    finalList := DeviceList{Content: dList, DeviceCount: count}
+    return &finalList
 }
